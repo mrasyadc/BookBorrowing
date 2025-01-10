dbed85e32069b47bc3dc290627e2580262476514
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2025-01-10 11:37:04 +0700

    add migration for books and books transaction

--- database/migrations/2025_01_10_042637_create_books_borrow_transaction_table.php
+++ database/migrations/2025_01_10_042637_create_books_borrow_transaction_table.php
@@ -12,8 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('books_borrow_transaction', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // Primary key
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // FK to users
+            $table->foreignId('book_id')->constrained('books')->onDelete('cascade'); // FK to books
+            $table->date('borrow_date');
+            $table->date('return_date');
+            $table->bigInteger('total_cost'); // Total biaya
+            $table->timestamps(); // created_at and updated_at
         });
     }
 
